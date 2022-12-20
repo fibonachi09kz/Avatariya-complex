9997529935765d52e1f6c78b4a9c12a37d3bb0fb
Aleksin Danila Sergeevich <69359758+fibonachi09kz@users.noreply.github.com>
2022-12-20 17:57:34 +0600

    upd

--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,9 @@ document.addEventListener('DOMContentLoaded', function() {
     
 
 
-
+    Fancybox.bind("[data-fancybox]", {
+    
+    });
 
 
 
---
+++ src/js/sliders.js
@@ -0,0 +1,47 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+    let windowSize = window.innerWidth;
+    console.log(windowSize)
+
+    var mainPromoSwiper = new Swiper(".main-promo-swiper", {
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+            bulletActiveClass: 'filled',
+            bulletClass: 'star-big',
+            renderBullet: function (index, className) {
+                return '<button type="button" class="' + className + '"></button>';
+            }
+        },
+        // effect: 'cube',
+        // cubeEffect: {
+        //     slideShadows: false,
+        // },
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
