abff8d38ac5fd6a754e6cea8529bd92aac44f252
Aleksin Danila Sergeevich <69359758+fibonachi09kz@users.noreply.github.com>
2022-12-23 17:49:23 +0600

    upload

--- src/js/main.js
+++ src/js/main.js
@@ -1,17 +1,29 @@
 document.addEventListener('DOMContentLoaded', function() {
     
-    const windowSize = window.innerWidth
 
+    // Большинство отдельных функций оборачиваем в модуль, чтобы иметь локальный namespace
+    //
+    // {
+    //     Код...
+    // }
+
+
+    // Определяем константы
+    const WINDOW_SIZE = window.innerWidth
+
+
+    // Инициализация Fancybox v4
     Fancybox.bind("[data-fancybox]", {
     
     });
 
 
 
-    // Fixed menu event control
+    // Плавающее меню в шапке
     {
         let elem = document.querySelector('#menu-layout');
         let menuPosition = elem.getBoundingClientRect().top;
+        elem.classList.remove('fixed')
         window.addEventListener('scroll', function() {
             if (scrollY > 0 && scrollY < menuPosition) {
                 elem.classList.remove('fixed')
@@ -21,6 +33,59 @@ document.addEventListener('DOMContentLoaded', function() {
             }
         });
     }
+
+    
+    // Раскрытие дополнительных преимуществ пакета в блоке тарифов на главной
+    {
+        let items = document.querySelectorAll('#tariffs .swiper .swiper-slide');
+        if (items) {
+            items.forEach(e => {
+                let features = e.querySelectorAll('.features li')
+                let expandLink = e.querySelector('a.expand');
+                if (features.length > 3) {
+                    expandLink.style.display = 'block';
+                    features.forEach((line, index) => {
+                        if (index > 2) {
+                            line.style.display = 'none'
+                        }
+                    })
+                } else {
+                    expandLink.style.display = 'none';
+                }
+                expandLink.addEventListener('click', function(e) {
+                    e.preventDefault();
+                    e.target.classList.toggle('open')
+                    if (e.target.classList.contains('open')) {
+                        features.forEach((line, index) => {
+                            line.style.display = 'block'
+                        })
+                        e.target.textContent = 'Скрыть'
+                    } else {
+                        features.forEach((line, index) => {
+                            if (index > 2) {
+                                line.style.display = 'none'
+                            }
+                        })
+                        e.target.textContent = 'Подробнее о пакете'
+                    }
+                })
+
+            })
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- src/js/sliders.js
+++ src/js/sliders.js
@@ -1,8 +1,12 @@
 document.addEventListener('DOMContentLoaded', function() {
 
-    let windowSize = window.innerWidth;
+    const WINDOW_SIZE = window.innerWidth;
 
-    var mainPromoSwiper = new Swiper(".main-promo-swiper", {
+
+
+    // Инициализация и конфиг отдельных слайдеров
+
+    let mainPromoSwiper = new Swiper(".main-promo-swiper", {
         pagination: {
             el: ".swiper-pagination",
             clickable: true,
@@ -11,12 +15,59 @@ document.addEventListener('DOMContentLoaded', function() {
             renderBullet: function (index, className) {
                 return '<button type="button" class="' + className + '"></button>';
             }
-        },
-        // effect: 'cube',
-        // cubeEffect: {
-        //     slideShadows: false,
-        // },
+        }
     });
+
+
+
+
+
+
+    // Boilerplate для слайдера, у которого навигация НЕ дочерний элемент .swiper [используется для вынесения навигации из контейнера и обход overflow:hidden]
+    // Конфиг инициализации расположен ниже
+
+    // <div class="swiper-control-wrapper">
+    //     <div class="swiper swiperName">
+    //         <div class="swiper-wrapper">
+    //             <div class="swiper-slide"></div>
+    //         </div>
+    //     </div>
+    //     <div class="swiper-pagination"></div>
+    //     <div class="swiper-button-next"></div>
+    //     <div class="swiper-button-prev"></div>
+    // </div>
+
+
+    let mainPageTariffsSwiper = new Swiper(".main-page-tariffs-swiper", {
+		slidesPerView: 4,
+		spaceBetween: 30,
+		grabCursor: true,
+		pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+            bulletActiveClass: 'filled',
+            bulletClass: 'star-big',
+            renderBullet: function (index, className) {
+                return '<button type="button" class="' + className + '"></button>';
+            }
+        },
+		observeParents: true,
+		on: {
+			init: function (swiper) {
+				swiper.el.querySelector('.swiper-preloader').remove()
+				swiper.params.navigation.nextEl = this.el.closest('.swiper-control-wrapper').querySelector('.swiper-button-next');
+				swiper.params.navigation.prevEl = this.el.closest('.swiper-control-wrapper').querySelector('.swiper-button-prev');
+				swiper.navigation.init()
+				swiper.navigation.update()
+			},
+			update: function (swiper) {
+				swiper.params.navigation.nextEl = this.el.closest('.swiper-control-wrapper').querySelector('.swiper-button-next');
+				swiper.params.navigation.prevEl = this.el.closest('.swiper-control-wrapper').querySelector('.swiper-button-prev');
+				swiper.navigation.update()
+			}
+		},
+	});
+
 
 
 
