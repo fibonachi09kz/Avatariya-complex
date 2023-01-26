edbe220d929280e257c720ad54e7b92898e6be54
Aleksin Danila Sergeevich <69359758+fibonachi09kz@users.noreply.github.com>
2023-01-26 15:42:06 +0600

    upload reviews-page

--- src/js/sliders.js
+++ src/js/sliders.js
@@ -242,6 +242,25 @@ document.addEventListener('DOMContentLoaded', function() {
 		},
 	});
 
+    let videoReviewsSwiper = new Swiper(".video-reviews-swiper", {
+		slidesPerView: 1,
+        spaceBetween: 40,
+		grabCursor: true,
+		on: {
+			init: function (swiper) {
+				swiper.params.navigation.nextEl = this.el.closest('.swiper-control-wrapper').querySelector('.swiper-button-next');
+				swiper.params.navigation.prevEl = this.el.closest('.swiper-control-wrapper').querySelector('.swiper-button-prev');
+				swiper.navigation.init()
+				swiper.navigation.update()
+			},
+			update: function (swiper) {
+				swiper.params.navigation.nextEl = this.el.closest('.swiper-control-wrapper').querySelector('.swiper-button-next');
+				swiper.params.navigation.prevEl = this.el.closest('.swiper-control-wrapper').querySelector('.swiper-button-prev');
+				swiper.navigation.update()
+			}
+		},
+	});
+
 
 
     let parkRulesSwiper = new Swiper(".park-rules-swiper", {
